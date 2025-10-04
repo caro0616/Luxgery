00dabb86c54efcbc2aba5f736101b24a501537a3
DCH <141684387+dechouriog@users.noreply.github.com>
2025-10-03 22:07:52 -0500

    First commit

---
+++ Luxgery/web/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('services/', views.services, name='services'),
+    path('about/', views.about, name='about'),
+    path('testimonios/', views.testimonios, name='testimonios'),
+    path('contacto/', views.contacto, name='contacto'),
+]
